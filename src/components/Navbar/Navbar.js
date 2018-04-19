c39a3caf6cf32d4dcca7deef2341a156d216de86
Joshua Bohde <bohdetroit@gmail.com>
2018-04-19 12:29:38 -0400

    moved img under nav

--- src/components/Navbar/Navbar.js
+++ src/components/Navbar/Navbar.js
@@ -2,12 +2,10 @@ import React from "react";
 import "./Navbar.css";
 
 const Navbar = props => 
-<nav className="navbar navbar-inverse" data-spy="affix" data-offset-top="197">
-  {/* <ul> */}
-        <div className="nav-item">Memory Game</div>
-        <div className="nav-item">{props.message}</div>
-        <div className="nav-item"><p className="card-text">Score: {props.counter} | Top Score: {props.topscore}</p></div>
-    {/* </ul> */}
+<nav className="navbar" data-spy="affix" data-offset-top="197">
+    <div className="nav-item">Memory Game</div>
+    <div className="nav-item">{props.message}</div>
+    <div className="nav-item"><p className="card-text">Score: {props.counter} | Top Score: {props.topscore}</p></div>
 </nav> 
 
 
