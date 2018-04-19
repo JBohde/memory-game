f1c9f3e1763916c138dd9c5fd486217f7a9522c1
Joshua Bohde <bohdetroit@gmail.com>
2018-04-19 12:43:49 -0400

    fixed container margin

--- src/components/Navbar/Navbar.js
+++ src/components/Navbar/Navbar.js
@@ -2,11 +2,10 @@ import React from "react";
 import "./Navbar.css";
 
 const Navbar = props => 
-<nav className="navbar" data-spy="affix" data-offset-top="197">
+<nav className="navbar">
     <div className="nav-item">Memory Game</div>
     <div className="nav-item">{props.message}</div>
     <div className="nav-item"><p className="card-text">Score: {props.counter} | Top Score: {props.topscore}</p></div>
 </nav> 
-
 
 export default Navbar;
