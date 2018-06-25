6b4b5b05b178f2b37c0251b5067c7322db908859
Joshua Bohde <bohdetroit@gmail.com>
2018-06-24 23:10:22 -0400

    more mobile responsive

--- src/components/CharacterCard/CharacterCard.js
+++ src/components/CharacterCard/CharacterCard.js
@@ -2,9 +2,9 @@ import React from "react";
 import "./CharacterCard.css";
 
 const CharacterCard = props => (
-  <div className="card">
+  <div className="col-xs-3 col-sm-3 col-md-4 col-lg-4 col-xl-4 card">
     <div className="img-container">
-      <img className="img character" alt={props.name} src={props.image} id={props.id} ispicked="false" onClick={props.handleIncrement}/>
+      <img className="img-responsive character" alt={props.name} src={props.image} id={props.id} ispicked="false" onClick={props.handleIncrement}/>
     </div>
   </div>
 );
--- src/components/Wrapper/Wrapper.js
+++ src/components/Wrapper/Wrapper.js
@@ -1,6 +1,6 @@
 import React from "react";
 import "./Wrapper.css";
 
-const Wrapper = props => <div className="container"><div id="myBox">{props.children}</div></div>;
+const Wrapper = props => <div className="row"><div id="myBox">{props.children}</div></div>;
 
 export default Wrapper;
