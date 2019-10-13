fa44bab52638f5981503586140d79541f4db9f59
Joshua Bohde <bohdecoded@gmail.com>
2019-10-12 20:06:32 -0400

    feat: new build

---
+++ build/precache-manifest.3117fc923f3e44309cb6494fc329dbbf.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "678edd2b01c81d716fca9bc7e1b4d7ea",
+    "url": "/index.html"
+  },
+  {
+    "revision": "91937352ee8218808ba2",
+    "url": "/static/css/main.46026f68.chunk.css"
+  },
+  {
+    "revision": "076383ae85066f82cfd5",
+    "url": "/static/js/2.f897f46c.chunk.js"
+  },
+  {
+    "revision": "91937352ee8218808ba2",
+    "url": "/static/js/main.c82be7eb.chunk.js"
+  },
+  {
+    "revision": "f903c460a2c87e54b293",
+    "url": "/static/js/runtime-main.c315beec.js"
+  },
+  {
+    "revision": "e154af7aa71bc471ce96a9ff663a7a3e",
+    "url": "/static/media/memory.e154af7a.jpg"
+  }
+]);
